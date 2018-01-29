b61e676e7592b3412a9d4defa16f6611a2ce764c
tatikpluto <tatik.pluto@gmail.com>
2018-01-29 20:52:57 +0100

    utf-8

--- RaceManagement/src/cz/pluto/tool/XmlTool.java
+++ RaceManagement/src/cz/pluto/tool/XmlTool.java
@@ -1,6 +1,12 @@
 package cz.pluto.tool;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -17,26 +23,43 @@ public class XmlTool {
         try {
             JAXBContext jaxbContext = JAXBContext.newInstance(Race.class);
             Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-
             // output pretty printed
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-            jaxbMarshaller.marshal(form.race, file);
+            try {
+                OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(file),"UTF-8");
+                jaxbMarshaller.marshal(form.race, out);
+            } catch (UnsupportedEncodingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
             // jaxbMarshaller.marshal(customer, System.out);
         } catch (JAXBException e) {
             e.printStackTrace();
         }
 
-        
+       
     }
-    
-    
-    
+   
+   
+   
     public static void importFromXML(RMForm form, File file) {
         try {
             JAXBContext jaxbContext = JAXBContext.newInstance(Race.class);
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-            form.race = (Race) jaxbUnmarshaller.unmarshal(file);
+            try {
+                InputStreamReader is = new InputStreamReader(new FileInputStream(file), "UTF-8");
+                form.race = (Race) jaxbUnmarshaller.unmarshal(is);
+            } catch (UnsupportedEncodingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+           
         } catch (JAXBException e) {
             e.printStackTrace();
         }
